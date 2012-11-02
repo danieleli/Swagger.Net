0f7f697b7b485baa2e8ffc9d733b162a0aaca05c
danieleli <danschlossberg@gmail.com>
2012-11-02 08:18:45 -0400

    move constants to independent file

---
+++ Swagger.Net/SwaggerConstants.cs
@@ -0,0 +1,13 @@
+namespace Swagger.Net
+{
+    public static class SwaggerConstants
+    {
+        public const string SWAGGER = "swagger";
+        public const string SWAGGER_VERSION = "2.0";
+        public const string FROMURI = "FromUri";
+        public const string FROMBODY = "FromBody";
+        public const string QUERY = "query";
+        public const string PATH = "path";
+        public const string BODY = "body";
+    }
+}
