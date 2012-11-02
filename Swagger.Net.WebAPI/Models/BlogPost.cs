8ed9e29f0309b1971c87952fdc13d072c4c3c17e
danieleli <danschlossberg@gmail.com>
2012-11-02 06:47:45 -0400

    replace docs folder with swaggerUI package

---
+++ Swagger.Net.WebAPI/Models/BlogPost.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Swagger.Net.WebApi.Models
+{
+    /// <summary>
+    /// Blog Post Class Summary
+    /// </summary>
+    /// <remarks>
+    /// Blog Post Class Remarks
+    /// </remarks>
+    public class BlogPost
+    {
+        /// <summary>
+        /// BlogPost.Title Summary
+        /// </summary>
+        /// <remarks>
+        /// BlogPost.Title Remarks
+        /// </remarks>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// BlogPost.Id Summary
+        /// </summary>
+        /// <remarks>
+        /// BlogPost.Id Remarks
+        /// </remarks>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// BlogPost.PublishDate Summary
+        /// </summary>
+        /// <remarks>
+        /// BlogPost.PublishDate Remarks
+        /// </remarks>
+        public DateTime PublishDate { get; set; }
+
+
+        /// <summary>
+        /// BlogPost.Author Summary
+        /// </summary>
+        /// <remarks>
+        /// BlogPost.Author Remarks
+        /// </remarks>
+        public string Author { get; set; }
+    }
+}
