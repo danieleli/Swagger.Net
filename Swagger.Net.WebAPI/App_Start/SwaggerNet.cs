9ebc0616e33e4b7c5fefd08e4cb95d198d1bf0cd
danieleli <danschlossberg@gmail.com>
2012-11-02 06:46:20 -0400

    configure swaggerActionFilter after setting DocumentProvider

--- Swagger.Net.WebAPI/App_Start/SwaggerNet.cs
+++ Swagger.Net.WebAPI/App_Start/SwaggerNet.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
 using System.Web.Http.Dispatcher;
 using System.Web.Routing;
+using System.Xml.XPath;
 using Swagger.Net;
 
 [assembly: WebActivator.PreApplicationStartMethod(typeof(Swagger.Net.WebApi.App_Start.SwaggerNet), "PreStart")]
@@ -25,18 +27,20 @@ namespace Swagger.Net.WebApi.App_Start
         public static void PostStart()
         {
             var config = GlobalConfiguration.Configuration;
-
-            config.Filters.Add(new SwaggerActionFilter());
-
+            
             try
             {
-                config.Services.Replace(typeof(IDocumentationProvider),
-                    new XmlCommentDocumentationProvider(HttpContext.Current.Server.MapPath("~/bin/Swagger.Net.WebApi.XML")));
+                var assemblyname = Assembly.GetAssembly(HttpContext.Current.ApplicationInstance.GetType().BaseType).GetName().Name;
+                var path = HttpContext.Current.Server.MapPath("~/bin/" + assemblyname + ".xml");
+                config.Services.Replace(typeof(IDocumentationProvider), new XmlCommentDocumentationProvider(path));
             }
             catch (FileNotFoundException)
             {
                 throw new Exception("Please enable \"XML documentation file\" in project properties with default (bin\\Swagger.Net.WebApi.XML) value or edit value in App_Start\\SwaggerNet.cs");
             }
+
+            config.Filters.Add(new SwaggerActionFilter());
+
         }
     }
 }
