301c13bf5631b001ced8b4cc7c60b5c60b0ee68e
Dan Schlossberg <danschlossberg@gmail.com>
2012-11-03 10:57:50 -0400

    use rhino mocks instead of .net 4.5 fakes.

--- Swagger.Net._Test/Factories/ApiFactory_Test.cs
+++ Swagger.Net._Test/Factories/ApiFactory_Test.cs
@@ -4,11 +4,12 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Web.Http.Controllers;
-using System.Web.Http.Controllers.Fakes;
 using System.Web.Http.Description;
 using System.Web.Http.Routing;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using Rhino.Mocks;
 using Swagger.Net.Factories;
 using Swagger.Net.Models;
 
@@ -27,8 +28,7 @@ namespace Swagger.Net._Test.Factories
         [TestMethod]
         public void Test()
         {
-            using (ShimsContext.Create())
-            {
+            
                 // Arrange
                 ApiDescription apiDesc = GetApiDescription();
                 var descriptions = new List<ApiDescription> {apiDesc};
@@ -39,37 +39,48 @@ namespace Swagger.Net._Test.Factories
 
                 // Asset (visually)
                 Debug.WriteLine(JsonConvert.SerializeObject(result));
-            }
+            
         }
 
         private static ApiDescription GetApiDescription()
         {
+            var actionDesc = CreateActionDescriptor();
+
             var apiDesc = new ApiDescription
-                              {
-                                  RelativePath = RELATIVE_PATH,
-                                  Documentation = DOCUMENTATION,
-                                  Route = new HttpRoute("fjdkl/ffdklsa/{cc}/{id}", new HttpRouteValueDictionary()),
-                                  HttpMethod = HttpMethod.Get
-                              };
+            {
+                RelativePath = RELATIVE_PATH,
+                Documentation = DOCUMENTATION,
+                Route = new HttpRoute("fjdkl/ffdklsa/{cc}/{id}", new HttpRouteValueDictionary()),
+                HttpMethod = HttpMethod.Get,
+                ActionDescriptor = actionDesc
+            };
 
-            StubHttpParameterDescriptor parameter = CreateParameter("pname", typeof (string), false);
-
-//            apiDesc.ActionDescriptor = new ShimHttpActionDescriptor();
-//            apiDesc.ActionDescriptor.GetParameters01 = () => new BindingList<HttpParameterDescriptor> {parameter};
-            apiDesc.ActionDescriptor.ControllerDescriptor = new ShimHttpControllerDescriptor {};
-            apiDesc.ActionDescriptor.ControllerDescriptor.ControllerName = CONTROLLER_NAME;
             return apiDesc;
         }
 
-        private static StubHttpParameterDescriptor CreateParameter(string name, Type type, bool isOptional)
+        private static HttpActionDescriptor CreateActionDescriptor()
         {
-            var parameter = new StubHttpParameterDescriptor
-                                {
-                                    ParameterNameGet = () => name,
-                                    ParameterTypeGet = () => type,
-                                    IsOptionalGet = () => isOptional
-                                };
-            return parameter;
+            var param = CreateParameter("pname", typeof (string), false);
+            var parameters = new BindingList<HttpParameterDescriptor> {param};
+
+            var actionDesc = MockRepository.GenerateStub<HttpActionDescriptor>();
+            actionDesc.Stub(x => x.GetParameters()).Return(parameters);
+
+            var ctlrDesc = MockRepository.GenerateStub<HttpControllerDescriptor>();
+            ctlrDesc.ControllerName= CONTROLLER_NAME;
+
+            actionDesc.ControllerDescriptor= ctlrDesc;
+            return actionDesc;
+        }
+
+        private static HttpParameterDescriptor CreateParameter(string name, Type type, bool isOptional)
+        {
+            var p = MockRepository.GenerateStub<HttpParameterDescriptor>();
+            p.Stub(x => x.ParameterName).Return(name);
+            p.Stub(x => x.ParameterType).Return(type);
+            p.Stub(x => x.IsOptional).Return(isOptional);
+            
+            return p;
         }
     }
 }
