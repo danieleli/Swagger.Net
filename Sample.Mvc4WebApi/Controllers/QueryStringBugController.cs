9c042cd68765a8bd0a104868ea10823ae2784977
Dan Schlossberg <danschlossberg@gmail.com>
2012-11-07 18:46:26 -0500

    comment out bug controller and add some links to page

--- Sample.Mvc4WebApi/Controllers/QueryStringBugController.cs
+++ Sample.Mvc4WebApi/Controllers/QueryStringBugController.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using Sample.Mvc4WebApi.Models;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+//using System.Web.Http;
+//using Sample.Mvc4WebApi.Models;
 
 
-namespace Swagger.Net.WebApi.Controllers
-{
-    /// <summary>
-    /// QueryStringBugController class summary
-    /// </summary>
-    /// <remarks>
-    /// QueryStringBugController class remarks
-    /// </remarks>
-    public class QueryStringBugController : ApiController
-    {
+//namespace Swagger.Net.WebApi.Controllers
+//{
+//    /// <summary>
+//    /// QueryStringBugController class summary
+//    /// </summary>
+//    /// <remarks>
+//    /// QueryStringBugController class remarks
+//    /// </remarks>
+//    public class QueryStringBugController : ApiController
+//    {
 
-        /// <summary>
-        /// QueryStringBugController.Get(Count) Summary
-        /// </summary>
-        /// <remarks>Here are some remarks about the Get by count</remarks>
-        public IEnumerable<int> Get(int count)
-        {
-            return new int[]{count,count};
-        }
+//        /// <summary>
+//        /// QueryStringBugController.Get(Count) Summary
+//        /// </summary>
+//        /// <remarks>Here are some remarks about the Get by count</remarks>
+//        public IEnumerable<int> Get(int count)
+//        {
+//            return new int[]{count,count};
+//        }
 
-    }
-}
+//    }
+//}
