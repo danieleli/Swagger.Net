dca4c52ba7f978bbaee1f4275bc7ac5467de586a
Dan Schlossberg <danschlossberg@gmail.com>
2012-11-09 11:47:47 -0500

    ensure /api/docs goes to swagger controller

--- Sample.Mvc4WebApi/Global.asax.cs
+++ Sample.Mvc4WebApi/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Sample.Mvc4WebApi.App_Start;
 
 namespace Sample.Mvc4WebApi
 {
@@ -18,10 +19,13 @@ namespace Sample.Mvc4WebApi
         {
             AreaRegistration.RegisterAllAreas();
 
+            SwaggerNet.Register(GlobalConfiguration.Configuration);
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            SwaggerNet.PostStart();
         }
     }
 }
