12bdbd6cb8287b52163c5d4ba3e826e2ed7d5afa
Dan Schlossberg <danschlossberg@gmail.com>
2012-11-11 08:53:14 -0500

    recreate Sample.WebApi and cleanup unused files

--- Sample.Mvc4WebApi/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sample.Mvc4WebApi.WebApiApplication" Language="C#" %>
---
+++ Sample.WebApi/Models/BlogPost.cs
@@ -0,0 +1,54 @@
+﻿    using System;
+    using System.Collections.Generic;
+    
+
+namespace Sample.WebApi.Models
+{
+    /// <summary>
+    /// Blog Post Class Summary
+    /// </summary>
+    /// <remarks>
+    /// Blog Post Class Remarks
+    /// </remarks>
+    public class BlogPost
+    {
+        public BlogPost()
+        {
+            SomeNumbers=new List<int>();
+            Data= new List<byte>().ToArray();
+        }
+        /// <summary>
+        /// BlogPost.Title Summary
+        /// </summary>
+        /// <remarks>
+        /// BlogPost.Title Remarks
+        /// </remarks>
+        public string Title { get; set; }
+        public Byte[] Data { get; set; }
+        /// <summary>
+        /// BlogPost.Id Summary
+        /// </summary>
+        /// <remarks>
+        /// BlogPost.Id Remarks
+        /// </remarks>
+        public int Id { get; set; }
+        public List<int> SomeNumbers { get; set; }
+        /// <summary>
+        /// BlogPost.PublishDate Summary
+        /// </summary>
+        /// <remarks>
+        /// BlogPost.PublishDate Remarks
+        /// </remarks>
+        public DateTime PublishDate { get; set; }
+
+        /// <summary>
+        /// BlogPost.Author (Person) Summary
+        /// </summary>
+        /// <remarks>
+        /// BlogPost.Author (Person) Remarks
+        /// </remarks>
+        public Person Author { get; set; }
+
+ 
+    }
+}
