1ad3b47cf4959cb1c33014856f2462275f87e256
Dan Schlossberg <danschlossberg@gmail.com>
2012-12-21 18:18:13 -0500

    remove commented out consts

--- Swagger.Net/G.cs
+++ Swagger.Net/G.cs
@@ -4,11 +4,6 @@ namespace Swagger.Net
     {
         public const string SWAGGER = "swagger";
         public const string SWAGGER_VERSION = "2.0";
-        //public const string FROMURI = "FromUri";
-        //public const string FROMBODY = "FromBody";
-        //public const string QUERY = "query";
-        //public const string PATH = "path";
-        //public const string BODY = "body";
     }
      
     public enum ParamType
