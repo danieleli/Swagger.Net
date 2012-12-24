6d9d18cd656586f7b4701e2b9cf27b307e957fc1
Dan Schlossberg <danschlossberg@gmail.com>
2012-12-24 13:56:19 -0500

    add params test

--- Swagger.Net._Test/Custom/Extensions/ActionExtensionsFixture.cs
+++ Swagger.Net._Test/Custom/Extensions/ActionExtensionsFixture.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Web.Http.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
 using Swagger.Net.Custom;
 using Swagger.Net.Custom.Extensions;
 
@@ -70,6 +72,14 @@ namespace Swagger.Net._Test.Custom.Extensions
         {
             Assert.Fail("not implemented");
         }
+        
+        [TestMethod]
+        public void GetParamsMeta()
+        {
+            var actionData = GetActionMeta();
+            Assert.AreEqual(1, actionData.Params.Count(), "Param count");
+        }
+
 
         const string ACTION_NAME = "GetFoo";
         const string PARENT_CONTROLLER_NAME = "Customer";
@@ -82,11 +92,16 @@ namespace Swagger.Net._Test.Custom.Extensions
         {
             var xPathQuery = string.Format(XPathQueries.METHOD,
                                            "Swagger.Net._Test.Custom.Extensions.Foo.GetFoo(System.Int32)");
-            
+            MockRepository repo = new MockRepository();
+
             var returnType = typeof(Foo);
             var docs = DocNavigator.Instance;
             var supportedMethod = HttpMethod.Get;
             var paramz = new List<HttpParameterDescriptor>();
+
+            var paramDescriptor = GetParam(repo);
+
+            paramz.Add(paramDescriptor);
 
             var actionMetadata = ActionExtensions.GetDocs(CONTROLLER_NAME, ACTION_NAME, returnType, PARENT_CONTROLLER_NAME, RELATIVE_PATH, supportedMethod, xPathQuery, docs, paramz);
 
@@ -94,5 +109,14 @@ namespace Swagger.Net._Test.Custom.Extensions
 
         }
 
+        private static HttpParameterDescriptor GetParam(MockRepository repo)
+        {
+            var paramDescriptor = repo.PartialMock<HttpParameterDescriptor>(null);
+            paramDescriptor.Stub(descriptor => descriptor.ParameterName).Return("id").Repeat.Any();
+            paramDescriptor.Stub(descriptor => descriptor.ParameterType).Return(typeof (int)).Repeat.Any();
+
+            paramDescriptor.Replay();
+            return paramDescriptor;
+        }
     }
 }
